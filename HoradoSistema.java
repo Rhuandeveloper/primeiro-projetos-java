8f9f362cf806c565c84539a54dc246496dae8b74
Rhuan Rodolfo <rhuanrodolfo13@gmail.com>
2026-01-16 19:42:39 -0300

    Update HoradoSistema.java

--- HoradoSistema.java
+++ HoradoSistema.java
@@ -4,5 +4,7 @@ public class HoradoSistema {
     public static void main(String[] args) {
         System.out.println();
         Date data = new Date();
+        System.out.println("a hora do sitema é:");
+        System.out.println(data.toString());
     }
 }
