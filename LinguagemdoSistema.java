ee6775688f6ab848e8e94d2bec16a38666ffefc1
Rhuan Rodolfo <rhuanrodolfo13@gmail.com>
2026-01-16 19:57:01 -0300

    Update LinguagemdoSistema.java

--- LinguagemdoSistema.java
+++ LinguagemdoSistema.java
@@ -1,3 +1,10 @@
+import java.util.Locale;
+
 public class LinguagemdoSistema {
-    
+    public static void main(String[] args) {
+        
+        System.out.println(" a linguagem do seu simtema é: ");
+        System.out.println(Locale.getDefault().getLanguage());
+        System.out.println(Locale.getDefault().getCountry());
+    }
 }
